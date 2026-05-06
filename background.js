95e9c0c031566460d732cecce38361c9833d7aa8
Manjunath C <manjunathmc1996@gmail.com>
2026-05-06 09:04:51 +0530

    this is done

--- background.js
+++ background.js
@@ -9,32 +9,63 @@ var loopinterval = null;
     });
 });*/
 
+chrome.runtime.onStartup.addListener(() => {
+    console.log("Starting up . . . !");
+    chrome.storage.local.get("status").then((st) => {
+        if(st.status)
+        {
+            chrome.storage.local.get("redirectlist").then((rl) => {
+                const redList = rl.redirectlist.data;
+                console.log(redList);
+                start(redList);
+            })
+        }
+    });
+});
+
+chrome.windows.onRemoved.addListener((windowID) => {
+    console.log("Closing down");
+    chrome.windows.getAll(function (window_list){
+        if(window_list.length == 0){
+        console.log("All windows have been shut down");
+        }
+        else
+        {
+            console.log(window_list.length);
+        }
+    });
+    
+});
 
 // listner to get stored data when the data changes
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     workersCopyBlockList = message;
     sendResponse({"status" : "all good"});
-    //console.log(workersCopyBlockList);
+    console.log(workersCopyBlockList);
 });
 
 //Listner for when the start or stop button in clicked
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.status == "start") {console.log("starting"); start(message.data);}
     else if(message.status == "stop") {console.log("stopping"); stop();}
+    else if(message.status == "test") {console.log("Testing");};
     sendResponse("all good");
 });
 
 //function to get the stored data when button is clicked
 function start(storedData)
 {
+    console.log(storedData);
     loopinterval = setInterval(myloop, 7000);
     workersCopyBlockList = JSON.parse(storedData) //some data we will be bringing forward. should be assigned here 
+    chrome.storage.local.set({"status": true}).then(() => {console.log("Status saved");});
 }
 
 function stop()
 {
     clearInterval(loopinterval);
     chrome.storage.local.set({"redirectlist": workersCopyBlockList}).then(() => {console.log("Data saved");});
+    chrome.storage.local.set({"status": false}).then(() => {console.log("Status saved");});
 }
 
 async function myloop()
@@ -58,9 +89,8 @@ function handleBlock(tab)
         if (workersCopyBlockList.redirectlist.includes(websiteName))
         {
             console.log("This is a blocked website : "+websiteName);
+            chrome.tabs.update(tab.id, {url : "https://www.google.com"});
         }
-
-        //continue here . . . . . .
 
     } catch (error) {
         console.log("URL Not found. Switch back to a browser tab");
